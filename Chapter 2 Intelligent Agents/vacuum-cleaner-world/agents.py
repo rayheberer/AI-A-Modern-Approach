8995818f5128d3d1e04c37e3af7e2771945178e2
Ray Heberer <ray.heberer@gmail.com>
2018-06-19 22:30:59 -0700

    Remove old directory

--- Chapter 2 Intelligent Agents/vacuum-cleaner-world/agents.py
+++
@@ -1,8 +0,0 @@
-class SimpleReflexAgent(object):
-    def decide(self, location, dirt):
-        if dirt:
-            return 'Clean'
-        if location.name == 'A':
-            return 'Right'
-        if location.name == 'B':
-            return 'Left'
